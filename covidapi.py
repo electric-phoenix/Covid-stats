0e0ba3e50b49aa347de58ccf07a22f81cd0556d0
Ephraim Emmanuel <57196382+electric-phoenix@users.noreply.github.com>
2020-09-18 19:04:53 +0100

    Add files via upload

---
+++ covidapi.py
@@ -0,0 +1,26 @@
+import json
+
+from urllib.request import urlopen
+
+with urlopen("https://api.covid19api.com/summary") as resp:
+    source = resp.read()
+
+stats = json.loads(source)
+Country = dict()
+for Nations in stats['Countries']:
+    name = Nations['Country']
+    new_cases = Nations['NewConfirmed']
+    total_confirmed = Nations['TotalConfirmed']
+    Country[name]=new_cases
+while True:
+    cname=input('Enter country: ')
+    if cname=='exit' or cname == 'Exit':
+        break 
+    try:
+        print("Today's cases for", cname,":", Country[cname])
+    except KeyError:
+        print("Invalid input\n Check your spelling, make sure the first letter is capitalised")    
+
+    
+
+
